9248d34b53a81ec43340cf5d0a78f31a10213175
KRIShark <cristofnagy@gmail.com>
2022-10-04 14:05:40 +0200

    u

--- src/BeamMp/GetBeamMpServers.cs
+++ src/BeamMp/GetBeamMpServers.cs
@@ -12,17 +12,17 @@ namespace GetBeamMpServersDCBot.data
 {
     public class GetBeamMpServers
     {
-        private string url;
+        private string _url;
         private readonly IConfigurationRoot _config;
 
         public GetBeamMpServers(IConfigurationRoot config)
         {
-            url = config["BeamMp:url"];
+            _url = config["BeamMp:url"];
         }
 
         public List<ServerInfo> GetServers() 
         {
-            var jsonString = new WebClient().DownloadString(url);
+            var jsonString = new WebClient().DownloadString(_url);
             List<ServerInfo> json = JsonSerializer.Deserialize<List<ServerInfo>>(jsonString);
             return json;
         }
